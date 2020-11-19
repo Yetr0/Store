27dc6fce825fb3983350ebbe19b666b067e700f2
Lucas Altesson <lucas.altesson@learnet.se>
2020-11-19 13:12:33 +0100

    La till Genre och Actor, ändringar i  Movie

--- DatabaseConnection/Model.cs
+++ DatabaseConnection/Model.cs
@@ -16,8 +16,28 @@ namespace DatabaseConnection
         public int Id { get; set; }
         public string Title { get; set; }
         public string ImageURL { get; set; }
+        public int ReleaseYear { get; set; }
+        public int AgeRestriction { get; set; }
+        public virtual List<Genre> Genres { get; set; }
+        public virtual List<Actor> Actors { get; set; }
         public virtual List<Rental> Sales { get; set; }
     }
+
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string GenreName { get; set; }
+        public virtual List<Movie> Movies { get; set; }
+    }
+
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public virtual List<Movie> Movies { get; set; }
+    }
+
     public class Rental
     {
         public int Id { get; set; }
